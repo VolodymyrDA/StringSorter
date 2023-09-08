0bd541a1b69b06ab32f31ce572a009f2e6d193d1
Volodymyr Doloka <optimyzator@gmal.com>
2023-09-08 13:46:42 +0300

    refactor to use streams

--- src/main/java/org/vdoloka/StringSorter.java
+++ src/main/java/org/vdoloka/StringSorter.java
@@ -1,26 +1,20 @@
 package org.vdoloka;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class StringSorter {
     public List<String> sortStrings(List<String> unsortedStrings, String exceptionChar) {
-        List<String> stringStartsWithExceptionChar = new ArrayList<>();
-        List<String> otherStrings = new ArrayList<>();
 
-        for (String str : unsortedStrings) {
-            if (str.startsWith(exceptionChar)) {
-                stringStartsWithExceptionChar.add(str);
-            } else {
-                otherStrings.add(str);
-            }
-        }
-
-        Collections.sort(otherStrings);
-        stringStartsWithExceptionChar.sort(Collections.reverseOrder());
-        otherStrings.addAll(stringStartsWithExceptionChar);
-
-        return otherStrings;
+        return Stream.concat(unsortedStrings.stream()
+                                .filter(str -> !str.startsWith(exceptionChar))
+                                .sorted()
+                        ,
+                        unsortedStrings.stream()
+                                .filter(str -> str.startsWith(exceptionChar))
+                                .sorted(Collections.reverseOrder())
+                )
+                .toList();
     }
 }
